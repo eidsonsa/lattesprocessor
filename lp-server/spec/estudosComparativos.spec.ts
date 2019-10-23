b44a41900af38b3f3e05ff4464486081478def35
Eidson Jacques Almeida de Sa <ejas@ug3c37.windows.cin.ufpe.br>
2019-10-23 11:43:04 -0300

    [#4]Criacao do teste de servidor dos estudos comparativos onde o ranking e vazio ate insercao de professores/artigos

---
+++ lp-server/spec/estudosComparativos.spec.ts
@@ -0,0 +1,22 @@
+//import { CadastroDeAlunos } from '../cadastrodealunos';
+//import { Aluno } from '../../common/aluno';
+
+describe("O estudo comparativo", () => {
+  var estudo: EstudoComparativo;
+
+  beforeEach(() => estudo = new EstudoComparativo())
+
+  it("tem pesos decrescentes se não for especificado", () => {
+    expect(estudo.a1 = 8);
+    expect(estudo.a2 = 7);
+    expect(estudo.b1 = 6);
+    expect(estudo.b2 = 5);
+    expect(estudo.b3 = 4);
+    expect(estudo.b4 = 3);
+    expect(estudo.b5 = 2);
+    expect(estudo.c = 1);
+  })
+
+ 
+
+}) 
---
+++ tests-acceptance/stepsdefinitions/estudosComparativos.ts
@@ -0,0 +1,33 @@
+import { defineSupportCode } from 'cucumber';
+import { browser, $, element, ElementArrayFinder, by } from 'protractor';
+let chai = require('chai').use(require('chai-as-promised'));
+let expect = chai.expect;
+
+defineSupportCode(function ({ Given, And, When, Then }) {
+    Given(/^Eu estou na página “Estudos Comparativos” $/, async () => {
+        await browser.get("http://localhost:4200/");
+        await expect(browser.getTitle()).to.eventually.equal('LattesProcessor');
+        await $("a[name='estudosComparativos']").click();
+    })
+
+    And(/^Eu escolho o método “critérios de avaliação personalizados”$/, async () => {
+        await $("input[name='CriteriosPersonalizados'").click();
+    });
+
+    And(/^O professor “Paulo” tem “3” artigos “A1”, 2 artigos “A2” e 1 artigo “B1””$/, async () => {
+        
+    });
+
+    And(/^O professor “Sílvio” tem “3” artigos “A1”, 2 artigo “A2” e 1 artigos “B4”$/, async () => {
+        
+    });
+
+    When(/^Eu gero o ranking$/, async () => {
+        await $("button[name='gerarRanking']").click();
+    });
+
+    Then(/^Paulo” fica na primeira linha com “44” pontos, por ordem de prioridade, e “Sílvio” na segunda com “29” pontos, por ordem de prioridade.$/, async () => {
+        
+    });
+
+}) 
