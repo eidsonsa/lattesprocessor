9b04a51b9ef1cfc0b73e79258852ee5c0813702b
Eidson <ejas@cin.ufpe.br>
2019-10-27 17:39:27 -0300

    [#1]Criacao do teste de unidade dos estudos comparativos com pesquisadores com quantidade diferente de artigos

--- lp-server/spec/estudosComparativos.spec.ts
+++ lp-server/spec/estudosComparativos.spec.ts
@@ -7,16 +7,32 @@ describe("O estudo comparativo", () => {
   beforeEach(() => estudo = new EstudoComparativo())
 
   it("tem pesos decrescentes se não for especificado", () => {
-    expect(estudo.a1 = 8);
-    expect(estudo.a2 = 7);
-    expect(estudo.b1 = 6);
-    expect(estudo.b2 = 5);
-    expect(estudo.b3 = 4);
-    expect(estudo.b4 = 3);
-    expect(estudo.b5 = 2);
-    expect(estudo.c = 1);
+    expect(estudo.a1).toBe(8);
+    expect(estudo.a2).toBe(7);
+    expect(estudo.b1).toBe(6);
+    expect(estudo.b2).toBe(5);
+    expect(estudo.b3).toBe(4);
+    expect(estudo.b4).toBe(3);
+    expect(estudo.b5).toBe(2);
+    expect(estudo.c).toBe(1);
   })
 
- 
+  it("inicia com nenhum pesquisador cadastrado"), () => {
+    expect(estudo.listaQtd.length).toBe(0);
+    expect(estudo.listaPesos.length).toBe(0);
+  }
+
+ it("entre pesquisadores com quantidades diferentes é decrescente"), () => {
+   var prof1: Professor = new Professor();
+   prof1.nome = 'Paulo';
+   prof1.qtd = 36;
+   var prof2: Professor = new Professor();
+   prof2.nome = 'Silvio';
+   prof2.qtd = 32;
+   estudo.adicionarProf(prof1);
+   estudo.adicionarProf(prof2);
+   expect(estudo.listaQtd[0].nome).toBe('Paulo');
+   expect(estudo.listaQtd[1].nome).toBe('Silvio');
+ })
 
 }) 
