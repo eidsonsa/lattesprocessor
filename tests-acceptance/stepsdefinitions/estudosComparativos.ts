519cdabac0347c281b3619eaaca7ebc026e6eb01
Eidson Jacques Almeida de Sa <ejas@WINDOWS.CIN.UFPE.BR>
2019-10-30 11:03:57 -0300

    [#2]Criacao do teste de aceitacao do cenario de comparacao de pesquisadores especificando os pesos pelo metodo de criterio

--- tests-acceptance/stepsdefinitions/estudosComparativos.ts
+++ tests-acceptance/stepsdefinitions/estudosComparativos.ts
@@ -10,11 +10,11 @@ defineSupportCode(function ({ Given, And, When, Then }) {
         await $("a[name='estudosComparativos']").click();
     })
 
-    And(/^O professor “Paulo” tem “3” artigos “A1”, 2 artigos “A2” e 1 artigo “B1””$/, async () => {
+    And(/^O professor "([^\"]*)" tem "(\d*)" artigos “A1”, "(\d*)" artigos “A2” e "(\d*)" artigo “B1”$/, async (prof, qtdA1, qtdA2, qtdB1) => {
         
     });
 
-    And(/^O professor “Sílvio” tem “3” artigos “A1”, 2 artigo “A2” e 1 artigos “B4”$/, async () => {
+    And(/^O professor "([^\"]*)" tem "(\d*)" artigos “A1”, "(\d*)" artigo “A2” e "(\d*)" artigos “B4”$/, async (prof, qtdA1, qtdA2, qtdB4) => {
         
     });
 
@@ -26,11 +26,11 @@ defineSupportCode(function ({ Given, And, When, Then }) {
         await $("button[name='gerarRanking']").click();
     });
 
-    Then(/^Paulo” fica na primeira linha com “44” pontos, por ordem de prioridade, e “Sílvio” na segunda com “29” pontos, por ordem de prioridade.$/, async () => {
+    Then(/^"([^\"]*)" fica na primeira linha com "(\d*)" pontos e "([^\"]*)" na segunda com "(\d*)" pontos, por ordem de prioridade.$/, async (prof1, pontos1, prof2, pontos2) => {
         
     });
 
-    And(/^O professor “Paulo” tem “36” artigos e o professor “Sílvio” tem “32” artigos”$/, async () => {
+    And(/^O professor "([^\"]*)" tem "(\d*)" artigos e o professor "([^\"]*)" tem "(\d*)" artigos”$/, async (prof1, qtd1, prof2, qtd2) => {
         
     });
 
@@ -38,7 +38,18 @@ defineSupportCode(function ({ Given, And, When, Then }) {
         await $("input[name='CriteriosPersonalizados'").click();
     });
 
-    Then(/^Then “Paulo” fica na primeira linha com “36” artigos e “Sílvio” na segunda com “32” artigos.$/, async () => {
+    Then(/^"([^\"]*)" fica na primeira linha com "(\d*)" artigos e "([^\"]*)" na segunda com "(\d*)" artigos.$/, async (prof1, qtd1, prof2, qtd2) => {
         
+    });
+
+    And(/^And eu atribuo os pesos "(\d*)" para “A1”, "(\d*)" para “A2”, "(\d*)" para “B1”, "(\d*)" para “B2”, "(\d*)" para “B3”, "(\d*)" para “B4”, "(\d*)" para “B5” e "(\d*)" para “C”$/, async(A1, A2, B1, B2, B3, B4, B5, C) => {
+        await $("input[name='a1']").sendKeys(A1);
+        await $("input[name='a2']").sendKeys(A2);
+        await $("input[name='b1']").sendKeys(B1);
+        await $("input[name='b2']").sendKeys(B2);
+        await $("input[name='b3']").sendKeys(B3);
+        await $("input[name='b4']").sendKeys(B4);
+        await $("input[name='b5']").sendKeys(B5);
+        await $("input[name='c']").sendKeys(C);
     });
 }) 
