a99944c0371e5b7c616e74d82dd432086d7af5b1
mrbelf <mbmt@cin.ufpe.br>
2019-12-10 20:48:26 -0300

    testes de aceitação de estudos comparativos

--- tests-acceptance/stepdefinitions/estudoscomparativos.ts
+++ tests-acceptance/stepdefinitions/estudoscomparativos.ts
@@ -1,81 +1,56 @@
 import { defineSupportCode } from 'cucumber';
-import { browser, $, element, ElementArrayFinder, by } from 'protractor';
-
-import { CadastroDePesquisadores } from '../../lp-server/cadastrodepesquisadores';
-import { Pesquisador } from '../../common/pesquisador';
-import { Publicacao } from '../../common/publicacao';
-
-let path = require('path');
+import { browser, $, element, ElementArrayFinder, by} from 'protractor';
+import { async } from 'q';
+import { Alert } from 'selenium-webdriver';
 let chai = require('chai').use(require('chai-as-promised'));
 let expect = chai.expect;
-
 let mesmoNome = ((elem, nome) => elem.element(by.name('nomesProfessores')).getText().then(text => text == nome))
 let mesmaQtd = ((elem, artigos) => elem.element(by.name('artigosProfessores')).getText().then(text => text == artigos))
 let pAND = ((p,q) => p.then(a => q.then(b => a && b)))
-
-function 
-
-
-defineSupportCode(function ({ Given, And, When, Then }) {
-    Given(/^Eu estou na página “Estudos Comparativos” $/, async () => {
-        await browser.get("http://localhost:4200/");
-        await expect(browser.getTitle()).to.eventually.equal('LattesProcessor');
-        await $("a[name='qualis']").click();
-        let filePath = path.join(__dirname, '/support_files/' + "publicacoes_2014.xls");
-        await $("input[name=QualisFile]").sendKeys(filePath);
-        await element(by.buttonText("Qualis Import")).click();
-        await $("a[name='estudosComparativos']").click();
+defineSupportCode(function ({ Given, When, Then }) {
+    Given('estou na página de "estudos comparativos"', async () => {
+        await browser.get("http://localhost:4200/estudoscomparativos");
+        await expect(browser.getTitle()).to.eventually.equal('Lattes Processor');
+        })
+    Given(/^o atributo "arquivo xml" está como "([^\"]*)"$/, async (xml_file) => {
+        var status_xml: ElementArrayFinder = element.all(by.name('status_xml'));
+	    await expect(status_xml.text).to.eventually.equal(xml_file);
+    });
+    When(/^eu seleciono a opção "critérios de avaliação personalizados"$/, async () => {
+        await $("a[id='pesoscheck']").click();
+    });
+    When(/^atribui os pesos "(\d+)",  "(\d+)", "(\d+)", "(\d+)", "(\d+)", "(\d+)", "(\d+)" e "(\w+)" respectivamente para "A1", "A2", "B1", "B2", "B3", "B4", "B5", "B5" e "C"$/, async (peso1,peso2,peso3,peso4,peso5,peso6,peso7,peso8) => {
+        await $("input[name='a1']").sendKeys(<string> peso1);
+        await $("input[name='a2']").sendKeys(<string> peso2);
+        await $("input[name='b1']").sendKeys(<string> peso3);
+        await $("input[name='b2']").sendKeys(<string> peso4);
+        await $("input[name='b3']").sendKeys(<string> peso5);
+        await $("input[name='b4']").sendKeys(<string> peso6);
+        await $("input[name='b5']").sendKeys(<string> peso7);
+        await $("input[name='c']").sendKeys(<string> peso8);
+    });
+    When(/^eu seleciono a "opção executar avaliação"$/, async () => {
+        await $("a[name='executar_avaliacao']").click();
+    });
+    
+    Then(/^eu vejo uma mensagem de que os valores para os pesos são inválidos$/, async () => {
+        let msg:Alert = browser.switchTo().alert();
+        await expect(msg.getText()).toEqual('Pesos inválidos');
+    });
+    Given('estou na página de "estudos comparativos"', async () => {
+        await browser.get("http://localhost:4200/estudoscomparativos");
+        await expect(browser.getTitle()).to.eventually.equal('Lattes Processor');
+    })
+    Given(/^o atributo "arquivo xml" está como "nenhum arquivo carregado"$/, async () => {
+        var status_xml: ElementArrayFinder = element.all(by.name("xlsausente"));
+	    await expect(status_xml.text).to.eventually.equal('nenhum arquivo carregado');
+    });
+    When(/^eu seleciono a opção "quantidade de artigos"$/, async () => {
+        await $("a[value='quantidade']").click();
+    });
+    Then(/^eu vejo uma mensagem informando que nenhum arquivo .xml foi carregado$/, async () => {
+        let msg:Alert = browser.switchTo().alert();
+        await expect(msg.getText()).toEqual('nenhum arquivo carregado');
     });
 
-    And(/^O professor "([^\"]*)" tem "(\d*)" artigos e o professor "([^\"]*)" tem "(\d*)" artigos”$/, async (prof1, qtd1, prof2, qtd2) => {
-        let cadastroPesq = new CadastroDePesquisadores();
-        let p1: Pesquisador = new Pesquisador();
-        p1.nome = prof1;
-        for (var i = 0; i < qtd1; i++){
-            let p: Publicacao = new Publicacao('a', 'a')
-            p1.addPublicacao(p);
-        }
-        let p2: Pesquisador = new Pesquisador();
-        p2.nome = prof2;
-        for (var i = 0; i < qtd2; i++){
-            let p: Publicacao = new Publicacao('a', 'a')
-            p2.addPublicacao(p);
-        }
-    });
-
-    When(/^Eu escolho o método “quantidade de artigos””$/, async () => {
-        await $("input[name='QuantidadeDeArtigos'").click();
-    });
-
-    And(/^Eu gero o ranking$/, async () => {
-        await $("button[name='gerarRanking']").click();
-    });
-
-    Then(/^"([^\"]*)" fica na primeira linha com "(\d*)" pontos e "([^\"]*)" na segunda com "(\d*)" pontos.$/, async (prof1, qtd1, prof2, qtd2) => {
-        var lista: ElementArrayFinder = element.all(by.name('listapesquisadores'));
-        await expect(lista[0].nome.to.eventually.equal(prof1));
-        await expect(lista[0].qtd.to.eventually.equal(qtd1));
-        await expect(lista[1].nome.to.eventually.equal(prof2));
-        await expect(lista[1].qtd.to.eventually.equal(qtd2));
-    });
-
-    And(/^O professor "([^\"]*)" tem "(\d*)" artigos “([^\"]*)”, "(\d*)" artigos “([^\"]*)” e "(\d*)" artigo “([^\"]*)”$/, async (prof, qtd1, peso1, qtd2, peso2, qtd3, peso3) => {
-        
-    });
-
-    When(/^Eu escolho o método “critérios de avaliação personalizados”$/, async () => {
-        await $("input[name='CriteriosPersonalizados'").click();
-    });
-
-    And(/^And eu atribuo os pesos "(\d*)", "(\d*)", "(\d*)", "(\d*)", "(\d*)", "(\d*)", "(\d*)", "(\d*)", respectivamente. $/, async(A1, A2, B1, B2, B3, B4, B5, C) => {
-        await $("input[name='a1']").sendKeys(A1);
-        await $("input[name='a2']").sendKeys(A2);
-        await $("input[name='b1']").sendKeys(B1);
-        await $("input[name='b2']").sendKeys(B2);
-        await $("input[name='b3']").sendKeys(B3);
-        await $("input[name='b4']").sendKeys(B4);
-        await $("input[name='b5']").sendKeys(B5);
-        await $("input[name='c']").sendKeys(C);
-    });
-
-}) 
+})  
